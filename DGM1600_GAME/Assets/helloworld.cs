c5518b2e3f8f32edae7d25764af1957d15f02720
Unknown <loganlemperle@yahoo.com>
2017-08-28 20:15:16 -0600

    New files

---
+++ DGM1600_GAME/Assets/helloworld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour {
+
+	string message = "help me";
+
+	// Use this for initialization
+	void Start () {
+	print("hello world");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(message);
+	}
+}
