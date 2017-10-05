e23336a15b0287199016f01b1b9d06a6d3a1e6aa
Unknown <loganlemperle@yahoo.com>
2017-10-04 20:06:53 -0600

    voids!!!

---
+++ DGM1600_GAME/Assets/voidfun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class voidfun : MonoBehaviour {
+	public int laserblast;
+
+	// Use this for initialization
+	void Start () {
+		Deathstar(laserblast);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Deathstar (int vent){
+		print ("Thats no moon thats a space station!");
+
+	if(vent == 1){
+			print("porkin's misses the mark!");
+		}
+	else if (vent == 2){
+	 print("red leader misses");
+	}
+
+	else{
+		print("death star blows uppppppp!!!!!!!");
+	}
+
+	}
+
+}
+
