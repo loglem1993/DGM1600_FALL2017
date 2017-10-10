2e64911addfb409da2aec3655579d353c590879c
Unknown <loganlemperle@yahoo.com>
2017-10-09 19:34:07 -0600

    functions

---
+++ DGM1600_GAME/Assets/functions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class functions : MonoBehaviour {
+
+	public void dostuff(){
+			print("Im doing stuff..");
+	}
+
+public int AddNumbers(int num1, int num2){
+		
+		int result = num1 + num2;
+
+		return result; 
+	}
+	void Start(){
+		dostuff();
+		int result = AddNumbers(5,11);
+		print(result);
+	}
+	
+} 
