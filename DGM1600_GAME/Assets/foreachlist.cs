f4c5456b41b92b61a307867b8ad79fcf4635c8cf
Unknown <loganlemperle@yahoo.com>
2017-10-11 20:10:38 -0600

    lists

---
+++ DGM1600_GAME/Assets/foreachlist.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foreachlist : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		List<firstorder> badguys = new List<firstorder>();
+
+		badguys.Add( new firstorder("snoke",1000));
+		badguys.Add( new firstorder("general hux",20));
+		badguys.Add( new firstorder("kylo ren", 400));
+		badguys.Add( new firstorder("captian phasma", 150));
+
+		badguys.Sort();
+
+		foreach(firstorder guy in badguys){
+			print(guy.name +" " +guy.power); 
+
+ 
+	}
+	
+
+		
+	}
+}
